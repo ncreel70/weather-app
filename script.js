edf948507d402e72d9a5ee525ff040ed6ed844f1
Noah Creel <noah.creel@gmail.com>
2021-04-16 23:22:20 -0600

    cleansing API_KEY

--- script.js
+++ script.js
@@ -3,14 +3,15 @@
 //   { mode: "cors" }
 // );
 
-async function getData(location) {
-  const API_KEY = "987a95061ac482195c723653a89a7a3e";
+import API_KEY from "./config";
+const getData = async (location) => {
   const response = await fetch(
-    `api.openweathermap.org/data/2.5/weather?q=${location}&appid=987a95061ac482195c723653a89a7a3e`,
+    `api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}`,
     { mode: "cors" }
   );
+
   const weatherData = await response.json();
   return console.log(response.json());
-}
+};
 
 getData("New York");
