3b4874c0a8b55d1a21c56f84b24c607c637012bd
noah <noah.creel@gmail.com>
2021-05-10 02:11:22 -0600

    verified API connection, started to grab data needed for base functionality

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-// fetch(
-//   `api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}`,
-//   { mode: "cors" }
-// );
+let API_KEY = config.API_KEY;
+let weather_icon = document.getElementById("weather_icon");
+let temperature = document.getElementById("temp")
 
-import API_KEY from "./config";
+
 const getData = async (location) => {
   const response = await fetch(
-    `api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}`,
+    `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=${API_KEY}`,
     { mode: "cors" }
   );
 
   const weatherData = await response.json();
-  return console.log(response.json());
+  return [weather_icon.src = `http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`,
+temperature.innerText = Math.round(weatherData.main.temp)];
 };
 
-getData("New York");
+getData("Cheyenne");
