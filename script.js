3f0df231b3d494d9af10b56843220e8ca3a70fe8
Noah Creel <noah.creel@gmail.com>
2021-05-12 23:19:04 -0600

    added search functionality, setting base for getting current location on load

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 let API_KEY = config.API_KEY;
-let weather_icon = document.getElementById("weather_icon");
-let temperature = document.getElementById("temp")
-
+let weatherIcon = document.getElementById("weather_icon");
+let temperature = document.getElementById("temp_value");
+let tempUnit = document.getElementById("temp_unit");
+let cityName = document.getElementById("city_name");
+let search = document.getElementById("search_box");
 
 const getData = async (location) => {
+  location = getSearch();
   const response = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=${API_KEY}`,
     { mode: "cors" }
   );
 
   const weatherData = await response.json();
-  return [weather_icon.src = `http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`,
-temperature.innerText = Math.round(weatherData.main.temp)];
+  return [
+    (weatherIcon.src = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`),
+    (temperature.innerText = Math.round(weatherData.main.temp)),
+    (cityName.innerText = weatherData.name),
+    (tempUnit.innerText = weatherData.temperature.unit),
+  ];
 };
 
-getData("Cheyenne");
+const getSearch = () => {
+  const searchInput = document.getElementById("search_box").value;
+  return searchInput;
+};
